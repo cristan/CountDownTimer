9526c4bb6aafcc271064630d8362ba205989daa3
Cristan Meijer <cristan.meijer@gmail.com>
2021-03-07 14:45:30 +0100

    Extract the 2 screens to 2 files

---
+++ app/src/main/java/com/example/androiddevchallenge/Coutdown.kt
@@ -0,0 +1,45 @@
+package com.example.androiddevchallenge
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieAnimationSpec
+import com.airbnb.lottie.compose.rememberLottieAnimationState
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun CountDown() {
+    Loader()
+}
+
+@Composable
+fun Loader() {
+    val animationSpec = remember { LottieAnimationSpec.RawRes(R.raw.loading_loop_animation_7743) }
+    val animationState =
+        rememberLottieAnimationState(autoPlay = true, repeatCount = Integer.MAX_VALUE)
+    LottieAnimation(
+        animationSpec,
+        animationState = animationState,
+        modifier = Modifier.size(100.dp)
+    )
+}
+
+@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@Composable
+private fun LightPreview() {
+    MyTheme {
+        CountDown()
+    }
+}
+
+@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+@Composable
+private fun DarkPreview() {
+    MyTheme(darkTheme = true) {
+        CountDown()
+    }
+}
---
+++ app/src/main/java/com/example/androiddevchallenge/DurationSetup.kt
@@ -0,0 +1,74 @@
+package com.example.androiddevchallenge
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.rememberNavController
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun DurationSetup(navController: NavHostController) {
+    Surface(color = MaterialTheme.colors.background) {
+        val (seconds, setSeconds) = remember { mutableStateOf(TextFieldValue("")) }
+        val (minutes, setMinutes) = remember { mutableStateOf(TextFieldValue("")) }
+        val (hours, setHours) = remember { mutableStateOf(TextFieldValue("")) }
+
+        Column(
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MyInputField(title = "Hours", text = hours, setText = setHours)
+                MyInputField(title = "Minutes", text = minutes, setText = setMinutes)
+                MyInputField(title = "Seconds", text = seconds, setText = setSeconds)
+                Spacer(modifier = Modifier.height(20.dp))
+                Button(
+                    onClick = { navController.navigate("countdown") }
+                ) {
+                    Text(text = "Start")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyInputField(title: String, text: TextFieldValue, setText: (TextFieldValue) -> Unit) {
+    OutlinedTextField(
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        singleLine = true,
+        value = text,
+        onValueChange = { setText(it) },
+        label = { Text(title) }
+    )
+}
+
+@Preview("Light Theme", widthDp = 360, heightDp = 640)
+@Composable
+private fun LightPreview() {
+    MyTheme {
+        DurationSetup(rememberNavController())
+    }
+}
+
+@Preview("Dark Theme", widthDp = 360, heightDp = 640)
+@Composable
+private fun DarkPreview() {
+    MyTheme(darkTheme = true) {
+        DurationSetup(rememberNavController())
+    }
+}
